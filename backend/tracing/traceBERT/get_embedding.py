7c507cd744607019ba88e323198ff22a1eba7938
root <root@DESKTOP-OVOQBN3>
2025-07-02 17:58:51 +0200

    project cleanup

--- backend/tracing/traceBERT/get_embedding.py
+++
@@ -1,5 +0,0 @@
-def get_embedding(text):
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-    with torch.no_grad():
-        embeddings = model(**inputs).pooler_output
-    return embeddings
--- backend/tracing/traceBERT/load_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
-
-# Load tokenizer and model
-model_dir = "./output_proto/siamese2_random_04-24 23-53-59_/final_model"
-tokenizer = AutoTokenizer.from_pretrained(model_dir)
-model = AutoModel.from_pretrained(model_dir)
-model.eval()
